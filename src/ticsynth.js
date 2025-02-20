099e622beaa036714aad7c85989e3e508b149984
Matt Westcott <matt@west.co.tt>
2025-02-20 02:38:51 +0000

    Implement multiple channels in TICSynth

--- src/ticsynth.js
+++ src/ticsynth.js
@@ -2,23 +2,28 @@ export class TICSynth {
     constructor(sampleRate) {
         this.sampleRate = sampleRate;
 
-        this.frequency = 440;
-        this.volume = 0;
-        this.waveform = [
-            15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        ];
-        this.waveformIsNoise = false;
         this.frameCallback = null;
         this.onFrame = null;
         this.frameNumber = 0;
 
         this.samplesPerFrame = sampleRate / 60;
         this.samplesToNextFrame = 0;
-        this.samplesToNextWaveformElement = 0;
-        this.samplesPerWaveformElement = 0;
-        this.waveformPtr = 0;
-        this.level = 0;
+
+        this.channels = [];
+        for (let i = 0; i < 4; i++) {
+            this.channels[i] = {
+                volume: 0,
+                waveform: [
+                    15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                ],
+                waveformIsNoise: false,
+                samplesToNextWaveformElement: 0,
+                samplesPerWaveformElement: 0,
+                waveformPtr: 0,
+                level: 0,
+            };
+        }
     }
     generate(audioData) {
         let samplePtr = 0;
@@ -28,40 +33,57 @@ export class TICSynth {
                     let frameData = this.frameCallback(this.frameNumber);
                     if (this.onFrame) this.onFrame(frameData);
                     this.frameNumber++;
-                    this.waveform = frameData[0].waveform;
-                    let waveformIsNoise = true;
-                    for (let i = 0; i < 32; i++) {
-                        if (this.waveform[i] != 0) {
-                            waveformIsNoise = false;
-                            break;
+                    for (let chan = 0; chan < 4; chan++) {
+                        const chanFrameData = frameData[chan];
+                        const channel = this.channels[chan];
+                        if (chanFrameData) {
+                            channel.waveform = chanFrameData.waveform;
+                            let waveformIsNoise = true;
+                            for (let i = 0; i < 32; i++) {
+                                if (channel.waveform[i] != 0) {
+                                    waveformIsNoise = false;
+                                    break;
+                                }
+                            }
+                            channel.waveformIsNoise = waveformIsNoise;
+                            channel.volume = chanFrameData.volume;
+                            const frequency = Math.floor(chanFrameData.frequency);
+                            channel.samplesPerWaveformElement = this.sampleRate / frequency / 32;
+                            if (channel.waveformIsNoise) {
+                                // fudge factor to match observed behaviour of TIC-80 -
+                                // apparently noise values are only picked half as often
+                                channel.samplesPerWaveformElement *= 2;
+                            }
+                        } else {
+                            channel.volume = 0;
                         }
                     }
-                    this.waveformIsNoise = waveformIsNoise;
-                    this.volume = frameData[0].volume;
-                    this.frequency = Math.floor(frameData[0].frequency);
                 } else {
-                    this.volume = 0;
+                    for (let chan = 0; chan < 4; chan++) {
+                        this.channels[chan].volume = 0;
+                    }
                 }
-                this.samplesPerWaveformElement = this.sampleRate / this.frequency / 32;
-                if (this.waveformIsNoise) {
-                    // fudge factor to match observed behaviour of TIC-80 -
-                    // apparently noise values are only picked half as often
-                    this.samplesPerWaveformElement *= 2;
-                }
+
                 this.samplesToNextFrame += this.samplesPerFrame;
             }
-            if (this.samplesToNextWaveformElement <= 0) {
-                if (this.waveformIsNoise) {
-                    this.level = Math.random() >= 0.5 ? 15 : 0;
-                } else {
-                    this.level = this.waveform[this.waveformPtr];
+
+            let combinedLevel = 0;
+
+            for (let chan = 0; chan < 4; chan++) {
+                const channel = this.channels[chan];
+                if (channel.samplesToNextWaveformElement <= 0) {
+                    if (channel.waveformIsNoise) {
+                        channel.level = Math.random() >= 0.5 ? 15 : 0;
+                    } else {
+                        channel.level = channel.waveform[channel.waveformPtr];
+                    }
+                    channel.waveformPtr = (channel.waveformPtr + 1) % 32;
+                    channel.samplesToNextWaveformElement += channel.samplesPerWaveformElement;
                 }
-                this.waveformPtr = (this.waveformPtr + 1) % 32;
-                this.samplesToNextWaveformElement += this.samplesPerWaveformElement;
+                channel.samplesToNextWaveformElement--;
+                combinedLevel += (channel.level - 7.5) / 7.5 * channel.volume / 15;
             }
-            let finalLevel = (this.level - 7.5) / 7.5 * this.volume / 15;
-            audioData[samplePtr++] = finalLevel;
-            this.samplesToNextWaveformElement--;
+            audioData[samplePtr++] = combinedLevel;
             this.samplesToNextFrame--;
         }
     }
